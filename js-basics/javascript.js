24317eb588b1506aeeac6e3c1fc31f89721d8b02
Zuna0hu <mythhuza@gmail.com>
2024-11-05 18:15:30 -0500

    Initialize js-test.html and javascript.js files
    Create js-test.html and javascript.js and test a log function in the
    JavaScript file. This is part of the exercise for JavaScript basics.

---
+++ js-basics/javascript.js
@@ -0,0 +1,14 @@
+console.log("Hello, World!");
+
+/You can change the value of variables declared by let/ 
+let name = "John";
+let surname = "Doe";
+let age = 11;
+
+console.log(name);
+console.log(surname);
+console.log(age);
+
+age = 54;
+console.log(age);
+
